c8a0567cf164bc19bf9a9bf94e93e2612013783f
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2017-06-21 12:03:59 +1000

    Now outputs wordlist.txt

--- text_processing/filter_text.py
+++ text_processing/filter_text.py
@@ -1,3 +1,8 @@
+""" Given a json file with transcript information this tools can perform 
+manipulations including generating word lists.
+
+Usage: python filter_text.py sample.json wordlist.txt
+"""
 
 import argparse
 import json
@@ -10,10 +15,29 @@ import os
 # Deal with things like <silence>
 
 
-def save_wordlist(data):
-	""" Given the
+def save_wordlist(wordlist, filename):
+	""" Given a list of strings write to file
 	"""
-	pass
+	try:
+		with open(filename, 'w') as f:
+			for word in wordlist:
+				f.write(word + '\n')
+	except:
+		print("Could not write out to file " + filename)
+		exit()
+
+
+def extract_wordlist(data):
+	""" Given the data object produce a list of strings of single words
+		Returned list is of unique words and sorted
+	""" 
+	result = []
+	for utt in data:
+		words = utt.get('transcript').split()
+		result.extend(words)
+	result = list(set(result))
+	result.sort()
+	return result
 	
 
 
@@ -28,7 +52,7 @@ def filter_data(data):
 			words = [word.replace(char, '') for word in words]
 		utt['transcript'] = ' '.join(words).lower() #words.join(' ').lower()
 
-		print(utt['transcript'])
+	#print(utt['transcript'])
 
 
 def load_file(filename):
@@ -38,7 +62,6 @@ def load_file(filename):
 		with open(filename) as data:
 			data = json.load(data)
 	except Exception as e:
-		print(e)
 		print("Could not read file " + filename)
 		exit()
 	return data
@@ -47,13 +70,17 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("infile", type=str,
 		help="The input file to clean.")
+	parser.add_argument("wordlistfile", type=str,
+		help="Output word list.")
 	args = parser.parse_args()
 
 	data = load_file(args.infile)
 
-	filter_data(data)
+	filter_data(data)  # mutates the data object
 
-	#save_data(data)
+	wordlist = extract_wordlist(data)
+
+	save_wordlist(wordlist, args.wordlistfile)
 
 
 
