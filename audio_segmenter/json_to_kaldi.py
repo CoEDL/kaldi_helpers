fbee167fea3ad5d4be057894de2cfd4d37a9e3f1
Mark Ellison <tmarkellison@gmail.com>
2017-07-20 13:53:04 +1000

    Adding sorting of output files to json_to_kaldi.py

--- audio_segmenter/json_to_kaldi.py
+++ audio_segmenter/json_to_kaldi.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 import json;
 import sys;
 import uuid;
@@ -30,12 +32,14 @@ utterances = {};
 if not os.path.exists(output_folder):
 	os.makedirs(output_folder);
 
-f_segments = open(output_folder + "/segments", "w", encoding="utf-8");
-f_transcripts = open(output_folder + "/text", "w", encoding="utf-8");
-f_speakers = open(output_folder + "/spk2gender", "w", encoding="utf-8");
-f_recordings = open(output_folder + "/wav.scp", "w", encoding="utf-8");
-f_utt2spk = open(output_folder + "/utt2spk", "w", encoding="utf-8");
-f_corpus = open(output_folder + "/corpus.txt", "w", encoding="utf-8");
+f_segments                      = open(output_folder + "/segments", "w", encoding="utf-8");
+f_transcripts                   = open(output_folder + "/text", "w", encoding="utf-8");
+f_speakers                      = open(output_folder + "/spk2gender", "w", encoding="utf-8");
+f_recordings                    = open(output_folder + "/wav.scp", "w", encoding="utf-8");
+f_utt2spk                       = open(output_folder + "/utt2spk", "w", encoding="utf-8");
+f_corpus                        = open(output_folder + "/corpus.txt", "w", encoding="utf-8");
+
+l_segments, l_transcripts, l_speakers, l_recordings, l_utt2spk, l_corpus = [], [], [], [], [], []
 
 for json_transcript in json_transcripts:
     transcript = json_transcript["transcript"];
@@ -46,25 +50,32 @@ for json_transcript in json_transcripts:
 
     if speakerId not in speakers:
         speakers[speakerId] = str(uuid.uuid4()); # create speaker id
-        f_speakers.write(speakers[speakerId] + " " + "f\n"); # writing gender
+        l_speakers.append(speakers[speakerId] + " " + "f\n"); # writing gender
 
     if audioFileName not in recordings:
         recordings[audioFileName] = str(uuid.uuid4()); # create recording id
-        f_recordings.write(recordings[audioFileName] + " " + audioFileName + "\n");
+        l_recordings.append(recordings[audioFileName] + " " + audioFileName + "\n");
 
-    speaker_id = speakers[speakerId];
-    recording_id = recordings[audioFileName];
-    utterance_id = speakers[speakerId] + "-" + str(uuid.uuid4());
+    speaker_id                  = speakers[speakerId];
+    recording_id                = recordings[audioFileName];
+    utterance_id                = speakers[speakerId] + "-" + str(uuid.uuid4());
 
     if silence_markers:
-      f_transcripts.write(utterance_id + " !SIL " + transcript + " !SIL\n");
+      l_transcripts.append(utterance_id + " !SIL " + transcript + " !SIL\n");
     else:
-      f_transcripts.write(utterance_id + " " + transcript + "\n");
-    f_segments.write(utterance_id + " " + recording_id + " " + "%f %f\n" % (startMs / 1000.0, stopMs / 1000.0));
+      l_transcripts.append(utterance_id + " " + transcript + "\n");
+    l_segments.append(utterance_id + " " + recording_id + " " + "%f %f\n" % (startMs / 1000.0, stopMs / 1000.0));
     #f_utt2spk.write(utterance_id + " " + speaker_id + "\n");
-    f_utt2spk.write(utterance_id + " " + utterance_id + "\n"); # hack to match utterances to utterances
-    f_corpus.write(transcript + "\n");
+    l_utt2spk.append(utterance_id + " " + utterance_id + "\n"); # hack to match utterances to utterances
+    l_corpus.append(transcript + "\n");
 
+
+l_segments.sort();     f_segments.write(   "".join(l_segments))
+l_transcripts.sort();  f_transcripts.write("".join(l_transcripts))
+l_speakers.sort();     f_speakers.write(   "".join(l_speakers))
+l_recordings.sort();   f_recordings.write( "".join(l_recordings))
+l_utt2spk.sort();      f_utt2spk.write(    "".join(l_utt2spk))
+l_corpus.sort();       f_corpus.write(     "".join(l_corpus))
 
 f_segments.close();
 f_transcripts.close();
