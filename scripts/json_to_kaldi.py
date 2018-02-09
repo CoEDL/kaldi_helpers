caf54fbab21f86aae799d5ad9611f40005940e83
scott-- <scott.heath@uqconnect.edu.au>
2018-02-09 13:06:22 +1000

    Split into training and testing sets (#23)
    * modified json_to_kaldi.py to divide data into test and training sets
    
    * modified Taskfile.yml to copy training and testing generated files
    
    * added some accidentally removed changes back in

--- scripts/json_to_kaldi.py
+++ scripts/json_to_kaldi.py
@@ -22,24 +22,86 @@ f_in = sys.stdin
 json_transcripts = json.loads(f_in.read())
 f_in.close()
 
-speakers = {}
-recordings = {}
-utterances = {}
-
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-l_segments, l_transcripts, l_speakers, l_recordings, l_utt2spk, l_corpus = [], [], [], [], [], []
+class KaldiInput:
+    def __init__(self, output_folder):
 
-f_segments = open(output_folder + "/segments", "w", encoding="utf-8")
-f_transcripts = open(output_folder + "/text", "w", encoding="utf-8")
-f_speakers = open(output_folder + "/spk2gender", "w", encoding="utf-8")
-f_recordings = open(output_folder + "/wav.scp", "w", encoding="utf-8")
-f_utt2spk = open(output_folder + "/utt2spk", "w", encoding="utf-8")
-f_corpus = open(output_folder + "/corpus.txt", "w", encoding="utf-8")
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        self.speakers = {}
+        self.recordings = {}
+        self.utterances = {}
+
+        self.l_segments, self.l_transcripts, self.l_speakers, self.l_recordings, self.l_utt2spk, self.l_corpus = [], [], [], [], [], []
+
+        self.f_segments = open(output_folder + "/segments", "w", encoding="utf-8")
+        self.f_transcripts = open(output_folder + "/text", "w", encoding="utf-8")
+        self.f_speakers = open(output_folder + "/spk2gender", "w", encoding="utf-8")
+        self.f_recordings = open(output_folder + "/wav.scp", "w", encoding="utf-8")
+        self.f_utt2spk = open(output_folder + "/utt2spk", "w", encoding="utf-8")
+        self.f_corpus = open(output_folder + "/corpus.txt", "w", encoding="utf-8")
+
+    def add_speaker_if_missing(self, speakerId):
+        if speakerId not in self.speakers:
+            # create speaker id
+            self.speakers[speakerId] = str(uuid.uuid4())
+            # writing gender
+            self.l_speakers.append(self.speakers[speakerId] + " " + "f\n")
+
+        return self.speakers[speakerId]
+
+    def add_recording_if_missing(self, audioFileName):
+        if audioFileName not in self.recordings:
+            # create recording id
+            self.recordings[audioFileName] = str(uuid.uuid4())
+            self.l_recordings.append(self.recordings[audioFileName] + " " + audioFileName + "\n")
+
+        return self.recordings[audioFileName]
+
+    def add(self, recording_id, utterance_id, startMs, stopMs, transcript, silence_markers):
+        if silence_markers:
+            self.l_transcripts.append(utterance_id + " !SIL " + transcript + " !SIL\n")
+        else:
+            self.l_transcripts.append(utterance_id + " " + transcript + "\n")
+        self.l_segments.append(utterance_id + " " + recording_id + " " + "%f %f\n" % (startMs / 1000.0, stopMs / 1000.0))
+        # f_utt2spk.write(utterance_id + " " + speaker_id + "\n")
+        # hack to match utterances to utterances
+        self.l_utt2spk.append(utterance_id + " " + utterance_id + "\n")
+        self.l_corpus.append(transcript + "\n")
+
+    def write_and_close(self):
+        self.l_segments.sort()
+        self.f_segments.write("".join(self.l_segments))
+        self.f_segments.close()
+
+        self.l_transcripts.sort()
+        self.f_transcripts.write("".join(self.l_transcripts))
+        self.f_transcripts.close()
+
+        self.l_speakers.sort()
+        self.f_speakers.write("".join(self.l_speakers))
+        self.f_speakers.close()
+
+        self.l_recordings.sort()
+        self.f_recordings.write("".join(self.l_recordings))
+        self.f_recordings.close()
+
+        self.l_utt2spk.sort()
+        self.f_utt2spk.write("".join(self.l_utt2spk))
+        self.f_utt2spk.close()
+
+        self.l_corpus.sort()
+        self.f_corpus.write("".join(self.l_corpus))
+        self.f_corpus.close()
 
 
-for json_transcript in json_transcripts:
+testing_input = KaldiInput(output_folder + "/testing")
+training_input = KaldiInput(output_folder + "/training")
+
+for i, json_transcript in enumerate(json_transcripts):
     transcript = json_transcript["transcript"]
     startMs = json_transcript["startMs"]
     stopMs = json_transcript["stopMs"]
@@ -51,52 +113,32 @@ for json_transcript in json_transcripts:
 
     audioFileName = json_transcript["audioFileName"].replace("\\", "/")
 
-    if speakerId not in speakers:
-        # create speaker id
-        speakers[speakerId] = str(uuid.uuid4())
-        # writing gender
-        l_speakers.append(speakers[speakerId] + " " + "f\n")
 
-    if audioFileName not in recordings:
-        # create recording id
-        recordings[audioFileName] = str(uuid.uuid4())
-        l_recordings.append(recordings[audioFileName] + " " + audioFileName + "\n")
 
-    speaker_id = speakers[speakerId]
-    recording_id = recordings[audioFileName]
-    utterance_id = speakers[speakerId] + "-" + str(uuid.uuid4())
+    #speaker_id = speakers[speakerId]
+    #recording_id = recordings[audioFileName]
+    #utterance_id = speakers[speakerId] + "-" + str(uuid.uuid4())
 
-    if silence_markers:
-        l_transcripts.append(utterance_id + " !SIL " + transcript + " !SIL\n")
+    if i % 10 == 0:
+        # add speaker id
+        speaker_id = testing_input.add_speaker_if_missing(speakerId)
+
+        # add audioFilename
+        recording_id = testing_input.add_recording_if_missing(audioFileName)
+
+        utterance_id = speaker_id + "-" + str(uuid.uuid4())
+
+        testing_input.add(recording_id, utterance_id, startMs, stopMs, transcript, silence_markers)
     else:
-        l_transcripts.append(utterance_id + " " + transcript + "\n")
-    l_segments.append(utterance_id + " " + recording_id + " " + "%f %f\n" % (startMs / 1000.0, stopMs / 1000.0))
-    # f_utt2spk.write(utterance_id + " " + speaker_id + "\n")
-    # hack to match utterances to utterances
-    l_utt2spk.append(utterance_id + " " + utterance_id + "\n")
-    l_corpus.append(transcript + "\n")
+        # add speaker id
+        speaker_id = training_input.add_speaker_if_missing(speakerId)
 
+        # add audioFilename
+        recording_id = training_input.add_recording_if_missing(audioFileName)
 
-l_segments.sort()
-f_segments.write("".join(l_segments))
-f_segments.close()
+        utterance_id = speaker_id + "-" + str(uuid.uuid4())
 
-l_transcripts.sort()
-f_transcripts.write("".join(l_transcripts))
-f_transcripts.close()
+        training_input.add(recording_id, utterance_id, startMs, stopMs, transcript, silence_markers)
 
-l_speakers.sort()
-f_speakers.write("".join(l_speakers))
-f_speakers.close()
-
-l_recordings.sort()
-f_recordings.write("".join(l_recordings))
-f_recordings.close()
-
-l_utt2spk.sort()
-f_utt2spk.write("".join(l_utt2spk))
-f_utt2spk.close()
-
-l_corpus.sort()
-f_corpus.write("".join(l_corpus))
-f_corpus.close()
+testing_input.write_and_close()
+training_input.write_and_close()
