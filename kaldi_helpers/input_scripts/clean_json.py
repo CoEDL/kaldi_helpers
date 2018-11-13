3106ffbaf38dde673e2a69663933771ac83f3ba8
nicklambourne <ndl93@live.com>
2018-11-13 17:39:53 +1000

    status to stdout regardless

--- kaldi_helpers/input_scripts/clean_json.py
+++ kaldi_helpers/input_scripts/clean_json.py
@@ -182,7 +182,7 @@ def main() -> None:
     write_data_to_json_file(data=list(filtered_data),
                             output=outfile)
 
-    print(f"Finished! Wrote {str(len(filtered_data))} transcriptions.", end="", flush=True, file=outfile)
+    print(f"Finished! Wrote {str(len(filtered_data))} transcriptions.", end="", flush=True)
 
 
 if __name__ == "__main__":
