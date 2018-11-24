8feae158263bb8d452b695e3da37dac1093ec33f
Ben Foley <ben@cbmm.io>
2018-11-24 21:41:22 +1100

    rename audio file earlier... shouldn't really matter

--- kaldi_helpers/inference_scripts/generate-infer-files.sh
+++ kaldi_helpers/inference_scripts/generate-infer-files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # prepare files needed for infer-align script
-# this expects test-train to have been run!
+# this expects test-train to have been run
+# single audio file only for now..
 
 # make ids
 
@@ -14,6 +15,12 @@ utt_id="$spk_id-utterance0"
 start_ms=0
 
 stop_ms=$(sox working_dir/input/infer/audio.wav -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p')
+
+
+
+# Rename the audio file to conform to expectations
+#
+mv working_dir/input/infer/*.wav working_dir/input/infer/audio.wav
 
 
 # write files
@@ -35,11 +42,6 @@ echo -e "$utt_id $rec_id $start_ms $stop_ms" > working_dir/input/infer/segments
 # Format is <recording_id> <path/to/audio.file>
 
 echo -e "decode data/infer/audio.wav" > working_dir/input/infer/wav.scp
-
-
-# Rename the audio file to conform to expectations
-#
-mv working_dir/input/infer/*.wav working_dir/input/infer/audio.wav
 
 
 # Clear out previous infer data in Kaldi
